5fba07f47a5b72801a055d161accefd409e2f4d5
Jason3dwebsite <119737054+Jason3dwebsite@users.noreply.github.com>
2025-04-10 21:13:04 -0400

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,10 +1,10 @@
 // urun.js
 document.addEventListener("keydown", function (e) {
-  if (e.key == "~" && e.ctrlKey) {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';
-    document.body.appendChild(script);
-
-    alert("Worked!");
+  if (e.key === "~" && e.ctrlKey) {
+    (function () {
+      var a = document.createElement('script');
+      a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';
+      document.body.appendChild(a);
+    })();
   }
 });
