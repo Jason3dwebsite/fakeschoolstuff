b77e678e4b036a62deb8defe70902be98900e541
Jason3dwebsite <119737054+Jason3dwebsite@users.noreply.github.com>
2025-04-10 21:55:03 -0400

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,7 +1,5 @@
 document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && e.shiftKey && e.key === "`") { // This is ~ when shift
-    var a = document.createElement('script');
-    a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';
-    document.body.appendChild(a);
+  if (e.ctrlKey && e.shiftKey && e.key === "`") {
+    alert("test");
   }
 });
